13a1024699113c9912e221d2bee5e2af241f2ef2
lowkieman <lowkieman@gmail.com>
2024-09-18 14:14:19 +0300

    update all react components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,3 +15,20 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const myFirstElement = <h1>Hello React!</h1>
+const mySecondElement= <p>This is a paragraph</p>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElement,mySecondElement);
+const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+root2.render(mySecondElement);
+
+/*
+You are now watching
+the React file 'index.js'
+through our 'Show React' tool.
+*/
